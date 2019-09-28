ecf005deb30124832b540d42c3a6b290fa24b4a8
Laurel <lak3r@mtmail.mtsu.edu>
2019-09-28 12:31:06 -0500

    Player fixed (hopefully)

---
+++ Physics Memes---The Game/Assets/Scripts/EndLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*
+public class EndLevel : MonoBehaviour
+{
+	public string nextScene;
+	private GameObject[] listOfBoxes = GameObject.FindGameObjectsWithTag("Box");
+
+	void OnCollisionEnter(Collision other){
+		if(listOfBoxes.Count == 0){
+			SceneManager.LoadScene(nextScene);
+		}
+	}
+}
+*/
