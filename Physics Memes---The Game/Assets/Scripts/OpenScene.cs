42871ca7d917e417f21bf8368588cfbf81c375b8
Laurel <lak3r@mtmail.mtsu.edu>
2019-09-28 10:50:40 -0500

    Fixed motion
    plus some other stuff don't worry about it

---
+++ Physics Memes---The Game/Assets/Scripts/OpenScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Inventory/List", order = 1)]
+//[MenuItem("Assets/Create/My Scriptable Object/OpenScene")]
+public class OpenScene : ScriptableObject
+{
+	public string nextScene;
+
+	public void NextScene(){
+		SceneManager.LoadScene(nextScene);
+	}	
+}
