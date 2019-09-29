ba7a30cb35ab75623dce1e72cde5ddd9fe04e86f
Matthew Ogden <ogdenm12@gmail.com>
2019-09-28 23:07:35 -0500

    Capacitor_Initial

---
+++ Physics Memes---The Game/Assets/Scripts/capacitorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class capacitorScript : MonoBehaviour
+{
+    private GameObject pObj;
+    private Vector3 pPos;
+    private Rigidbody pRb;
+    private PlayerControl pS;
+    private Vector3 startPos;
+
+    private float chargeSrength;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pPos = pObj.transform.position;
+        startPos = pObj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pPos = pObj.transform.position;
+
+        Debug.Log(pPos);
+        // If Cow falls below map
+        if (pObj.transform.position.y < -3)
+
+        {
+            pObj.transform.position = startPos;
+            pRb.velocity = new Vector3(0, 0, 0);
+            pRb.angularVelocity = new Vector3(0, 0, 0);
+        }
+
+    }
+}
